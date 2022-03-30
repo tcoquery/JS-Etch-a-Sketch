510e7e46f602d62a2f8724230815d37fa7acdc66
tcoquery <tcoquery@gmail.com>
2022-03-30 18:31:41 +0200

    make the grid size customizable

--- javascript.js
+++ javascript.js
@@ -1,39 +1,33 @@
 const container = document.getElementById("container");
 const playAgain = document.querySelector(".playAgain");
 
+let squareNumbers = Number(window.prompt("Set a size for the grid (must be less than 100)")); 
+
 function createGrid(x) {
     for (let rows = 0; rows < x; rows++) {
         for (let columns = 0; columns < x; columns++) {
             const grid = document.createElement("div");
             grid.classList.add("grid");
+            grid.style.height = 960/x + "px";
+            grid.style.width = 960/x + "px";
             container.appendChild(grid);
         };
     }
 }
 
-createGrid(16);
+createGrid(squareNumbers);
 
 const gridItem = document.querySelectorAll(".grid");
 
 gridItem.forEach(grid =>{
     grid.addEventListener("mouseover", function() {
-        grid.setAttribute("style", "background: black;");
+        grid.style.backgroundColor = "black";
+
     });
 });
 
 playAgain.addEventListener("click", function(){
     gridItem.forEach(grid =>{
-            grid.setAttribute("style", "background: white;");
+        grid.style.backgroundColor = "white";
         });
 });
-
-
-
-
-
-
-
-
-
-
-
